f8147ac87c841e638d85d848b540c078f5529ca6
infracom <c.santacruza@uniandes.edu.co>
2020-02-27 13:27:57 -0500

    Drawer, Theme Colors and Fonts OK!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff4D0084),
+        accentColor: Color(0xff852EAD),
+        buttonColor: Color(0xff7ACEEF),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 5.0,
+          color: Color(0xff4D0084),
+        ),
+        fontFamily: 'Roboto',
+      ),
       home: Home(),
     );
   }
@@ -24,14 +38,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  bool micro = false;
+  bool micro = true;
   int _page = 0;
   GlobalKey _bottomNavKey = GlobalKey();
+  bool _swipe = false;
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
+
+    TextStyle _textStyle = Theme.of(context).textTheme.display1; 
+    
+    return Scaffold(
+        drawer: Drawer(child: Icon(Icons.person_pin,color: Colors.black,),elevation: 5.3,),
+        appBar: AppBar(
+          centerTitle: true,
+            title: Text("Unilago",
+            style: _textStyle,),
+        ),
         body: Container(
           color: Colors.white,
           height: MediaQuery.of(context).size.height,
@@ -39,34 +63,38 @@ class _HomeState extends State<Home> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text("This is ${_page.toString()}",
-              textScaleFactor: 10.0,
-                style: TextStyle(
-                  fontSize: 18.0,
-                ),
+              Text(
+                _page.toString(),
+                textScaleFactor: 5.0,
+                style: TextStyle(fontFamily: 'Ovo'),
               ),
               RaisedButton(
-                  child:Text("Go to page of index 1"),
-                  onPressed: (){
-                    final CurvedNavigationBarState navBarState = _bottomNavKey.currentState;
-                    navBarState.setPage(1);
-                  },
+                child: Text(
+                  "Go to page of index 1",
                 ),
+                onPressed: () {
+                  final CurvedNavigationBarState navBarState =
+                      _bottomNavKey.currentState;
+                  navBarState.setPage(1);
+                },
+              ),
             ],
           ),
         ),
         bottomNavigationBar: CurvedNavigationBar(
           key: _bottomNavKey,
           items: [
-            Icon(Icons.menu),
-            Icon(Icons.mic,color: micro==true ? Colors.white:Colors.black,),
+            Icon(Icons.favorite),
+            Icon(
+              Icons.mic,
+              color: micro == true ? Theme.of(context).accentColor : Colors.white,
+            ),
             Icon(Icons.keyboard),
           ],
-          color: Colors.deepPurpleAccent,
+          color: Theme.of(context).primaryColor,
           backgroundColor: Colors.white,
-          buttonBackgroundColor: micro==true 
-          ? Colors.blue[900]
-          : Colors.blue[100],
+          buttonBackgroundColor:
+              micro == true ? Theme.of(context).buttonColor : Theme.of(context).buttonColor,
           height: 47.0,
           animationDuration: Duration(
             milliseconds: 300,
@@ -75,18 +103,20 @@ class _HomeState extends State<Home> {
           index: 1,
           onTap: (index) {
             if (index == 1) {
-              setState((){micro = true;});
+              setState(() {
+                micro = true;
+              });
             } else {
-              setState((){micro = false;});
+              setState(() {
+                micro = false;
+              });
             }
 
             setState(() {
               _page = index;
             });
-
           },
         ),
-      ),
     );
   }
 }
